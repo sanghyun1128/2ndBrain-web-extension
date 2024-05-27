477e91d246e7e51c553d6b80a2c7abb950a570af
sanghyun1128 <sanghyun1128@gmail.com>
2024-05-27 22:20:01 +0900

    feat: add new list item and store it in local storage

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,5 +1,6 @@
 const textInput = document.getElementById("textInput");
 const enterButton = document.getElementById("addButton");
+const list = document.getElementById("list");
 let index = 0;
 
 if (textInput && enterButton) {
@@ -13,15 +14,53 @@ if (textInput && enterButton) {
   enterButton.addEventListener("click", addListItem);
 }
 
+//TODO: localStorage에 저장된 데이터가 무작위 순서로 나오는 문제
+if (list) {
+  for (let i = 0; i < localStorage.length; i++) {
+    if (localStorage.key(i).includes("2nd_brain_")) {
+      const listItem = document.createElement("li");
+      const itemText = document.createElement("span");
+      const deleteButton = document.createElement("button");
+
+      deleteButton.textContent = "DEL";
+      deleteButton.className = "delButton";
+      deleteButton.id = "delButton_" + i;
+      deleteButton.onclick = function () {
+        list.removeChild(listItem);
+        localStorage.removeItem(localStorage.key(i));
+      };
+      itemText.textContent = localStorage.getItem(localStorage.key(i));
+      listItem.appendChild(itemText);
+      listItem.appendChild(deleteButton);
+
+      list.appendChild(listItem);
+    }
+  }
+}
+
 function addListItem() {
   console.log("addListItem");
   const text = textInput.value;
   if (text) {
     localStorage.setItem("2nd_brain_" + index, text);
-    index++;
-    // const listItem = document.createElement("li");
-    // listItem.textContent = text;
-    // document.getElementById("list").appendChild(listItem);
+
+    const listItem = document.createElement("li");
+    const itemText = document.createElement("span");
+    const deleteButton = document.createElement("button");
+
+    deleteButton.textContent = "DEL";
+    deleteButton.className = "delButton";
+    deleteButton.id = "delButton_" + index;
+    deleteButton.onclick = function () {
+      list.removeChild(listItem);
+      localStorage.removeItem("2nd_brain_" + deleteButton.id.split("_")[1]);
+    };
+    itemText.textContent = text;
+    listItem.appendChild(itemText);
+    listItem.appendChild(deleteButton);
+
+    list.appendChild(listItem);
     textInput.value = "";
+    index++;
   }
 }

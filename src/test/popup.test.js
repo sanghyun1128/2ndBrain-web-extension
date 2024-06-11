7f53ed477df07466dfbad1413b40941ee51ebe5f
sanghyun1128 <sanghyun1128@gmail.com>
2024-06-11 14:16:06 +0900

    chore: Update test.yml to install Chrome and dependencies for Puppeteer

--- src/test/popup.test.js
+++ src/test/popup.test.js
@@ -14,8 +14,7 @@ describe("2ndBrain Chrome Extension popup", () => {
   beforeAll(async () => {
     const extensionPath = path.resolve(__dirname, "../..");
     browser = await puppeteer.launch({
-      executablePath: "/usr/bin/chromium-browser",
-      headless: true,
+      headless: false,
       args: [
         `--disable-extensions-except=${extensionPath}`,
         `--load-extension=${extensionPath}`,

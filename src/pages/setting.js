82c13fe1f94afa383ea582f98461b40df93b3602
sanghyun1128 <sanghyun1128@gmail.com>
2024-06-18 21:02:31 +0900

    feat: make alternating display of setting pages and history pages

--- src/pages/setting.js
+++ src/pages/setting.js
@@ -1,23 +1,39 @@
 window.onload = () => {
-  const list = document.getElementById("list");
+  const historyButton = document.getElementById("historyButton");
+  const settingButton = document.getElementById("settingButton");
 
   /**
-   * chrome.storage.local에 저장된 데이터를 불러와서 createListItem 함수를 호출
-   * - chrome.storage.local에 저장된 데이터를 불러와서 삭제된 시간을 기준으로 내림차순 정렬
-   * - key가 2ndBrain_item__로 시작하고 deletedTime이 null이 아닌 경우에만 createListItem 함수를 호출
+   * query에 따라서 setting 또는 history를 보여줌
+   * - query가 setting인 경우 settingHeader와 settingList를 보여주고 historyHeader와 historyList를 숨김
    */
-  if (list) {
-    chrome.storage.local.get(null, (items) => {
-      Object.entries(items)
-        .filter(
-          ([key, value]) =>
-            key.includes("2ndBrain_item__") && value.deletedTime !== null
-        )
-        .sort((a, b) => b[1].deletedTime - a[1].deletedTime)
-        .forEach(([key, value]) => {
-          const itemAddTimeMs = parseInt(key.split("__")[1]);
-          createListItem(value);
-        });
+  const urlParams = new URLSearchParams(window.location.search);
+  const query = urlParams.get("page");
+  if (query === "setting") {
+    showSetting();
+    historyButton.style.backgroundColor = "#196861";
+    settingButton.style.backgroundColor = "#28a398";
+  } else {
+    showHistory();
+    historyButton.style.backgroundColor = "#28a398";
+    settingButton.style.backgroundColor = "#196861";
+  }
+
+  /**
+   * historyButton 클릭 시 history를 보여주고 settingButton 클릭 시 setting을 보여주는 이벤트 리스너 등록
+   * - historyButton 클릭 시 showHistory 함수 호출
+   * - settingButton 클릭 시 showSetting 함수 호출
+   */
+  if (historyButton && settingButton) {
+    historyButton.addEventListener("click", () => {
+      showHistory();
+      historyButton.style.backgroundColor = "#28a398";
+      settingButton.style.backgroundColor = "#196861";
+    });
+
+    settingButton.addEventListener("click", () => {
+      showSetting();
+      historyButton.style.backgroundColor = "#196861";
+      settingButton.style.backgroundColor = "#28a398";
     });
   }
 };
@@ -84,4 +100,46 @@ const convertQueryKeyToSiteName = (queryKey) => {
   };
 
   return siteName[queryKey] || "Unknown";
+};
+
+const showHistory = (historyButton, settingButton) => {
+  const listHeader = document.getElementById("listHeader");
+  const list = document.getElementById("list");
+  const settingHeader = document.getElementById("settingHeader");
+  const settingList = document.getElementById("settingList");
+
+  listHeader.style.display = "block";
+  list.style.display = "block";
+  settingHeader.style.display = "none";
+  settingList.style.display = "none";
+
+  /**
+   * chrome.storage.local에 저장된 데이터를 불러와서 createListItem 함수를 호출
+   * - chrome.storage.local에 저장된 데이터를 불러와서 삭제된 시간을 기준으로 내림차순 정렬
+   * - key가 2ndBrain_item__로 시작하고 deletedTime이 null이 아닌 경우에만 createListItem 함수를 호출
+   */
+  chrome.storage.local.get(null, (items) => {
+    Object.entries(items)
+      .filter(
+        ([key, value]) =>
+          key.includes("2ndBrain_item__") && value.deletedTime !== null
+      )
+      .sort((a, b) => b[1].deletedTime - a[1].deletedTime)
+      .forEach(([key, value]) => {
+        const itemAddTimeMs = parseInt(key.split("__")[1]);
+        createListItem(value);
+      });
+  });
+};
+
+const showSetting = () => {
+  const listHeader = document.getElementById("listHeader");
+  const list = document.getElementById("list");
+  const settingHeader = document.getElementById("settingHeader");
+  const settingList = document.getElementById("settingList");
+
+  listHeader.style.display = "none";
+  list.style.display = "none";
+  settingHeader.style.display = "block";
+  settingList.style.display = "block";
 };

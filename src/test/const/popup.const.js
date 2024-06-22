5957fb8e015291098bcbb927cab6b8d404e8abd5
sanghyun1128 <sanghyun1128@gmail.com>
2024-06-22 14:10:01 +0900

    feat: update warning text constants for multiple languages

--- src/test/const/popup.const.js
+++ src/test/const/popup.const.js
@@ -1,15 +1,21 @@
 const MAX_MEMO_SIZE = 10;
 const MAX_DELETED_MEMO_SIZE = 100;
 const MAX_INPUT_LENGTH = 30;
-const WARNING_TEXT = {
+const WARNING_TEXT_KO = {
   MAX_MEMO_SIZE_WARNING: `메모는 최대 ${MAX_MEMO_SIZE}개까지 저장 가능합니다.`,
   MAX_INPUT_LENGTH_WARNING: `메모는 ${MAX_INPUT_LENGTH}자 이내로 입력해주세요.`,
   EMPTY_INPUT_WARNING: "메모를 입력해주세요.",
+};
+const WARNING_TEXT_EN = {
+  MAX_MEMO_SIZE_WARNING: `You can save up to ${MAX_MEMO_SIZE} memos.`,
+  MAX_INPUT_LENGTH_WARNING: `Please enter a memo within ${MAX_INPUT_LENGTH} characters.`,
+  EMPTY_INPUT_WARNING: "Please enter a memo.",
 };
 
 module.exports = {
   MAX_MEMO_SIZE,
   MAX_DELETED_MEMO_SIZE,
   MAX_INPUT_LENGTH,
-  WARNING_TEXT,
+  WARNING_TEXT_KO,
+  WARNING_TEXT_EN,
 };

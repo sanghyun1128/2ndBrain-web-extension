1764046f2807a2edf2328a35b0d5d0d98ee4ff57
sanghyun1128 <sanghyun1128@gmail.com>
2024-06-22 15:02:45 +0900

    feat: add constants for setting page names and button labels in multiple languages

---
+++ src/const/setting.const.js
@@ -0,0 +1,22 @@
+export const HISTORY_COLUMN_NAME_KO = {
+  CONTENT: "내용",
+  MATCHED_TEXT: "일치된 내용",
+  DELETED_BY: "검색한 곳",
+  ADDED_TIME: "추가한 날짜",
+  DELETED_TIME: "삭제한 날짜",
+};
+export const HISTORY_COLUMN_NAME_EN = {
+  CONTENT: "Content",
+  MATCHED_TEXT: "Matched Text",
+  DELETED_BY: "Deleted By",
+  ADDED_TIME: "Added Time",
+  DELETED_TIME: "Deleted Time",
+};
+export const SETTING_PAGE_NAME_KO = "설정 페이지";
+export const SETTING_PAGE_NAME_EN = "Setting Page";
+export const SETTING_CLEAR_ALL_BUTTON_KO = "저장된 기록 모두 비우기";
+export const SETTING_CLEAR_ALL_BUTTON_EN = "Clear all history";
+export const SETTING_THEME_SELECT_KO = "테마 선택";
+export const SETTING_THEME_SELECT_EN = "Select theme";
+export const SETTING_LANGUAGE_SELECT_KO = "언어 선택";
+export const SETTING_LANGUAGE_SELECT_EN = "Select language";
